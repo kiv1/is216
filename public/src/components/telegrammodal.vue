7904bff54e7597af80a53e30a033f003ed112d03
Muhd Zaki <zaki.jst@gmail.com>
2021-11-02 19:54:01 +0800

    Update form layout

--- public/src/components/telegrammodal.vue
+++ public/src/components/telegrammodal.vue
@@ -12,8 +12,8 @@
                     <div class="modal-body">
                         <form class="form">
                             <div class="row">
+                                <label>Telegram Handle</label>
                                 <div class="input-group mb-3">
-                                    <label class="input-group-text">Telegram Handle</label>
                                     <input
                                         type="text"
                                         class="form-control"
@@ -30,31 +30,34 @@
                                         >Verify</button
                                     >
                                 </div>
-                                <div v-if="hasUserClickVerify" class="input-group mb-3">
-                                    <label class="input-group-text">OTP</label>
-                                    <input
-                                        type="text"
-                                        class="form-control"
-                                        placeholder="OTP"
-                                        v-model="userOTP"
-                                        required
-                                        :disabled="usernameVerified"
-                                    />
-                                    <button
-                                        type="button"
-                                        :disabled="usernameVerified"
-                                        @click="verifyUsername"
-                                        class="btn btn-primary float-end"
-                                        >Resend</button
-                                    >
-                                    <button
-                                        type="button"
-                                        :disabled="!userOTP || usernameVerified"
-                                        @click="verifyOTP"
-                                        class="btn btn-success float-end"
-                                        >Confirm</button
-                                    >
-                                </div>
+                                <template v-if="hasUserClickVerify">
+                                    <label>OTP</label>
+
+                                    <div class="input-group mb-3">
+                                        <input
+                                            type="text"
+                                            class="form-control"
+                                            placeholder="OTP"
+                                            v-model="userOTP"
+                                            required
+                                            :disabled="usernameVerified"
+                                        />
+                                        <button
+                                            type="button"
+                                            :disabled="usernameVerified"
+                                            @click="verifyUsername"
+                                            class="btn btn-primary float-end"
+                                            >Resend</button
+                                        >
+                                        <button
+                                            type="button"
+                                            :disabled="!userOTP || usernameVerified"
+                                            @click="verifyOTP"
+                                            class="btn btn-success float-end"
+                                            >Confirm</button
+                                        >
+                                    </div>
+                                </template>
                             </div>
                         </form>
                     </div>
