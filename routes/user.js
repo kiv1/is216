8934fb7d7c2d9d79681a44f8cd43027960830bbd
Muhd Zaki <zaki.jst@gmail.com>
2021-11-05 17:18:57 +0800

    Update setup page title

--- routes/user.js
+++ routes/user.js
@@ -84,6 +84,10 @@ router.get('/mybuddy', checkCookie, (req, res) => {
     res.sendFile(path.join(__dirname, '../public/views/user', 'mybuddy.html'), { dotfiles: 'allow' });
 });
 
+router.get('/setup', checkCookie, (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/views/user', 'setup.html'), { dotfiles: 'allow' });
+});
+
 function checkCookie(req, res, next) {
     const sessionCookie = req.cookies.session || '';
     firebaseLL
